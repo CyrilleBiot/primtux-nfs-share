e138d99d4619c35409cdd131d7d76b97b15020fd
Cyrille Biot <cyrille@cbiot.fr>
2021-02-20 15:17:40 +0100

    Fixed bugs

--- primux-nfs-share-server.sh
+++ primux-nfs-share-server.sh
@@ -18,45 +18,29 @@ if [[ $(aptitude search nfs-kernel-server | cut -d " " -f 1) = "i" ]]
 	else
 		echo "nfs-kernel-server n'est pas installé."
 		echo "Installation du paquet nfs-kernel-server."
-		#apt install nfs-kernel-server
+		apt install nfs-kernel-server
 fi
 
 
 # Préparation des partages sur le serveur
-# mkdir /home/01-mini/partage-mini
-# mkdir /home/02-super/partage-super
-# mkdir /home/03-maxi/partage-maxi
-# mdir  /home/administrateur/partage-mini
-# mdir  /home/administrateur/partage-super
-# mdir  /home/administrateur/partage-maxi
-# mkdir /home/administrateur/partage-administrateur
+mkdir /home/01-mini/partage-mini
+mkdir /home/02-super/partage-super
+mkdir /home/03-maxi/partage-maxi
+mkdir /home/administrateur/partage-mini
+mkdir /home/administrateur/partage-super
+mkdir /home/administrateur/partage-maxi
+mkdir /home/administrateur/partage-administrateur
 
 
 # Configuration du fichier exports
 # Partage lecture / ecriture
-echo "/home/01-mini/partage-mini /home/01-mini/partage-mini" >> /etc/exports
-echo "/home/02-super/partage-super /home/02-super/partage-super"  >> /etc/exports
-echo "/home/03-maxi/partage-maxi /home/03-maxi/partage-maxi"  >> /etc/exports
-echo "/home/01-mini/partage-mini /home/administrateur/partage-mini"  >> /etc/exports
-echo "/home/02-super/partage-super /home/administrateur/partage-super"  >> /etc/exports
-echo "/home/administrateur/partage-administrateur $ipMini.1/24(rw,sync)" >> /etc/exports
-echo "/home/03-maxi/partage-maxi /home/administrateur/partage-maxi"  >> /etc/exports
+echo "/home/01-mini/partage-mini /home/01-mini/partage-mini $ipMini.1/24(rw,sync)" rw,users 0 0 >> /etc/exports
+echo "/home/02-super/partage-super /home/02-super/partage-super $ipMini.1/24(rw,sync)"  >> /etc/exports
+echo "/home/03-maxi/partage-maxi /home/03-maxi/partage-maxi $ipMini.1/24(rw,sync)"  >> /etc/exports
+echo "/home/01-mini/partage-mini /home/administrateur/partage-mini $ipMini.1/24(rw,sync)"  >> /etc/exports
+echo "/home/02-super/partage-super /home/administrateur/partage-super $ipMini.1/24(rw,sync)"  >> /etc/exports
+echo "/home/administrateur/partage-administrateur $ipMini.1/24(rw,sync) $ipMini.1/24(rw,sync)" >> /etc/exports
+echo "/home/03-maxi/partage-maxi /home/administrateur/partage-maxi $ipMini.1/24(rw,sync)"  >> /etc/exports
 
-
-# Insertion dans fstab
-echo "$ipServeur:/home/01-mini/partage-mini /home/01-mini/partage-mini" >> /etc/fstab
-echo "$ipServeur:/home/02-super/partage-super /home/02-super/partage-super"  >> /etc/fstab
-echo "$ipServeur:/home/03-maxi/partage-maxi /home/03-maxi/partage-maxi"  >> /etc/fstab
-echo "$ipServeur:/home/01-mini/partage-mini /home/administrateur/partage-mini"  >> /etc/fstab
-echo "$ipServeur:/home/02-super/partage-super /home/administrateur/partage-super"  >> /etc/fstab
-echo "$ipServeur:/home/03-maxi/partage-maxi /home/administrateur/partage-maxi"  >> /etc/fstab
-echo "$ipServeur:/home/administrateur/partage-administrateur /home/administrateur/partage-administrateur"  >> /etc/fstab
-
-mount /home/01-mini/partage-mini
-mount /home/02-super/partage-super
-mount /home/03-maxi/partage-maxi
-mount /home/administrateur/partage-mini
-mount /home/administrateur/partage-super
-mount /home/administrateur/partage-maxi
-mount /home/administrateur/partage-administrateur
-
+# Redémarrage des services
+service nfs-kernel-server restart
