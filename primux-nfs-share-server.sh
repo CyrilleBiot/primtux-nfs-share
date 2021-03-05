3941d8fb545ad092c60db6e4ffabbfbc3313550a
Cyrille <44138670+CyrilleBiot@users.noreply.github.com>
2021-03-05 22:21:29 +0100

    Update primux-nfs-share-server.sh

--- primux-nfs-share-server.sh
+++ primux-nfs-share-server.sh
@@ -29,23 +29,25 @@ if [[ $1 = "clean" ]]
   then
 	# Confirmer la suppression de la configuration
 	read -p "Attention. Voulez réellement supprimer la config nfs serveur (oui / non) ?" answer
-	if echo "$answer" | grep -i "^o" ;then
-		# On efface la configuration
-		rm -rf /home/01-mini/partage-mini
-		rm -rf /home/02-super/partage-super
-		rm -rf /home/03-maxi/partage-maxi
-		rm -rf /home/administrateur/partage-administrateur
+	if echo "$answer" | grep -i "^o" 
+		then
+			# On efface la configuration
+			rm -rf /home/01-mini/partage-mini
+			rm -rf /home/02-super/partage-super
+			rm -rf /home/03-maxi/partage-maxi
+			rm -rf /home/administrateur/partage-administrateur
+			
+			# On rétablit les fichiers d'origine
+			sed -i '/#PrimtuxNFS/{N;N;N;N;N;N;N;d;}' /etc/exports
+			sed -i '/#PrimtuxNFS/{N;N;N;N;d;}' /etc/hosts.allow 
+
 		
-		# On rétablit les fichiers d'origine
-		sed -e '/# Partage NFS Primtux/{N;N;N;N;N;N;N;N;d}' /etc/exports
-		sed -e '/# Partage NFS Primtux/{N;N;N;N;N;d}' /etc/hosts.allow
-		
-		# On redémarre les services
-		exportfs -a
-		systemctl restart nfs-kernel-server
-		exit 1
+			# On redémarre les services
+			exportfs -a
+			systemctl restart nfs-kernel-server
+			exit			
 	else
-		exit 1
+		exit
 	fi
 fi
 
@@ -62,7 +64,7 @@ fi
       		        		echo "nfs-kernel-server n'est pas installé."
       		        		echo "Installation du paquet nfs-kernel-server."
       		        		apt install nfs-kernel-server
-      		        		fi
+      		        	fi
 
 							# Création d'un nouveau groupe primtux-nfs
 							# Et ajout des users
@@ -73,7 +75,7 @@ fi
 							adduser  03-maxi primtux-nfs
 							
 							# On récup l'UID du groupe (normalement 1004 mais va t on savoir...)              
-							# GUID=$(cat /etc/group | grep primtux-nfs | cut -d ":" -f 3)
+							GUID=$(cat /etc/group | grep primtux-nfs | cut -d ":" -f 3)
 
       		        		# Préparation des partages sur le serveur
       		        		mkdir /home/01-mini/partage-mini
@@ -89,24 +91,24 @@ fi
 
       		        		# Configuration du fichier exports
       		        		# Partage lecture / ecriture
-      		        		echo "# Partage NFS Primtux" >> /etc/exports
+      		        		echo "#PrimtuxNFS" >> /etc/exports
       		        		echo "#" >> /etc/exports
       		        		echo "/home/01-mini/partage-mini $ipMini.1/24(rw,sync,all_squash,anonuid=1001,anongid=$GUID)"  >> /etc/exports
       		        		echo "/home/02-super/partage-super $ipMini.1/24(rw,sync,all_squash,anonuid=1002,anongid=$GUID)"  >> /etc/exports
       		        		echo "/home/03-maxi/partage-maxi $ipMini.1/24(rw,sync,all_squash,anonuid=1003,anongid=$GUID)"  >> /etc/exports
-      		        		echo "/home/administrateur/partage-administrateur $ipMini.1/24(rw,sync,,all_squash,anonuid=1000,anongid=$GUID)" >> /etc/exports
+      		        		echo "/home/administrateur/partage-administrateur $ipMini.1/24(rw,sync,all_squash,anonuid=1000,anongid=$GUID)" >> /etc/exports
       		        		echo "#" >> /etc/exports
       		        		echo "# ===========================================" >> /etc/exports			
       		        							
       		        		# Configuration fichier hosts.allow
       		        		# Droit sur réseau local
-      		        		echo "# Partage NFS Primtux" >> /etc/hosts.allow
+      		        		echo "#PrimtuxNFS" >> /etc/hosts.allow
       		        		echo "#" >> /etc/hosts.allow
       		        		echo "ALL:LOCAL" >> /etc/hosts.allow
       		        		echo "#" >> /etc/hosts.allow
       		        		echo "# ===========================================" >> /etc/hosts.allow
       		        							
 								
-   							# Redémarrage des services
-   							exportfs -a
-  							systemctl restart nfs-kernel-server
+   					# Redémarrage des services
+   					exportfs -a
+  					systemctl restart nfs-kernel-server
